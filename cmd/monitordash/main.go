92d6b9d6350489eed02a1d44e399e59a22390930
Coral <101916048+Coralnws@users.noreply.github.com>
2025-08-04 20:12:42 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/monitordash/main.go
@@ -0,0 +1,23 @@
+// cmd/monitordash/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"monitordash/internal/monitordash"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := monitordash.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
